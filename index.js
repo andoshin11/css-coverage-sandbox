21550c4c272513de272ceda8bb45b890b0bd636a
Shin Ando <shinglish11@gmail.com>
2019-06-20 16:23:10 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer')
+const pti = require('puppeteer-to-istanbul')
+
+async function main() {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.coverage.startCSSCoverage({ resetOnNavigation: false })
+  // await page.coverage.startJSCoverage()
+  await page.goto('http://www.photocreate.co.jp/company/overview/')
+
+  const cssCoverage = await page.coverage.stopCSSCoverage()
+  // const jsCoverage = await page.coverage.stopJSCoverage()
+  const data = cssCoverage.map(coverage => {
+    coverage.url = coverage.url.replace('?', 'symbol')
+    return coverage
+  })
+  // const jsData = jsCoverage.map(coverage => {
+  //   coverage.url = coverage.url.replace('?', 'symbol')
+  //   return coverage
+  // })
+  pti.write(data)
+  // pti.write(jsData)
+
+  browser.close()
+}
+
+main()
